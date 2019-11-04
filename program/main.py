de25c5a1cfb9674985cdf549aca158827a1a0627
Andrea Solis <39932150+SolAndrea@users.noreply.github.com>
2019-11-03 19:03:20 -0500

    Update main.py

--- program/main.py
+++ program/main.py
@@ -6,6 +6,7 @@ def main():
     IMPORTANT: Change this file path to change training data 
     """
     file = open('BananasTraining.csv')
+    #file = open('TomatoesTraining.csv')
     """
     IMPORTANT: Change this variable too change target attribute 
     """
@@ -21,45 +22,53 @@ def main():
     tree = DecisionTree.makeTree(data, attributes, target, 0)
     print( "generated decision tree")
     #Generate program
-    file = open('program.py', 'w')
-    file.write("import Node\n\n")
+    file = open('programBananas.py', 'w')
+    #file = open('programTomatoes.py', 'w')
+    file.write("import Node\n")
+    file.write("import kivy\n")
+    file.write("from kivy.app import App\n")
+    file.write("from kivy.uix.label import Label\n")
+    file.write("import globalfile\n\n")
+	
+    file.write("def main():\n")
     #open input file
-    file.write("data = [[]]\n")
+    file.write("\tdata = [[]]\n")
     """
     IMPORTANT: Change this file path to change testing data 
     """
-    file.write("f = open('bananas.csv')\n")
+    file.write("\tf = open('bananas.csv')\n")
+    #file.write("\tf = open('Tomatoes.csv')\n")
     #gather data
-    file.write("for line in f:\n\tline = line.strip(\"\\r\\n\")\n\tdata.append(line.split(','))\n")
-    file.write("data.remove([])\n")
+    file.write("\tfor line in f:\n\t\tline = line.strip(\"\\r\\n\")\n\t\tdata.append(line.split(','))\n")
+    file.write("\tdata.remove([])\n")
     #input dictionary tree
-    file.write("tree = %s\n" % str(tree))
-    file.write("attributes = %s\n" % str(attributes))
-    file.write("count = 0\n")
-    file.write("for entry in data:\n")
-    file.write("\tcount += 1\n")
+    file.write("\ttree = %s\n" % str(tree))
+    file.write("\tattributes = %s\n" % str(attributes))
+    file.write("\tcount = 0\n")
+    file.write("\tfor entry in data:\n")
+    file.write("\t\tcount += 1\n")
     #copy dictionary
-    file.write("\ttempDict = tree.copy()\n")
-    file.write("\tresult = \"\"\n")
+    file.write("\t\ttempDict = tree.copy()\n")
+    file.write("\t\tresult = \"\"\n")
     #generate actual tree
-    file.write("\twhile(isinstance(tempDict, dict)):\n")
-    file.write("\t\troot = Node.Node(tempDict.keys()[0], tempDict[tempDict.keys()[0]])\n")
-    file.write("\t\ttempDict = tempDict[tempDict.keys()[0]]\n")
+    file.write("\t\twhile(isinstance(tempDict, dict)):\n")
+    file.write("\t\t\troot = Node.Node(tempDict.keys()[0], tempDict[tempDict.keys()[0]])\n")
+    file.write("\t\t\ttempDict = tempDict[tempDict.keys()[0]]\n")
     #this must be attribute
-    file.write("\t\tindex = attributes.index(root.value)\n")
-    file.write("\t\tvalue = entry[index]\n")
+    file.write("\t\t\tindex = attributes.index(root.value)\n")
+    file.write("\t\t\tvalue = entry[index]\n")
     #ensure that key exists
-    file.write("\t\tif(value in tempDict.keys()):\n")
-    file.write("\t\t\tchild = Node.Node(value, tempDict[value])\n")
-    file.write("\t\t\tresult = tempDict[value]\n")
-    file.write("\t\t\ttempDict = tempDict[value]\n")
+    file.write("\t\t\tif(value in tempDict.keys()):\n")
+    file.write("\t\t\t\tchild = Node.Node(value, tempDict[value])\n")
+    file.write("\t\t\t\tglobalfile.resultRIPE = tempDict[value]\n")
+    file.write("\t\t\t\ttempDict = tempDict[value]\n")
     #otherwise, break
-    file.write("\t\telse:\n")
-    file.write("\t\t\tprint \"can't process input %s\" % count\n")
-    file.write("\t\t\tresult = \"?\"\n")
-    file.write("\t\t\tbreak\n")
+    file.write("\t\t\telse:\n")
+    file.write("\t\t\t\tprint \"can't process input %s\" % count\n")
+    file.write("\t\t\t\tresult = \"?\"\n")
+    file.write("\t\t\t\tbreak\n")
     #print solutions 
-    file.write("\tprint (\"entry%s = %s\" % (count, result))\n")
+    file.write("\t\tprint (\"entry%s = %s\" % (count, globalfile.resultRIPE))\n")
     print("written program")
     
     
