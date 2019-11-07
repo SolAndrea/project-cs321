aa2904b519518c868d915fffe8794b30176c9753
Andrea Solis <39932150+SolAndrea@users.noreply.github.com>
2019-11-07 11:06:55 -0500

    Update programBananas.py

--- programBananas.py
+++ programBananas.py
@@ -11,7 +11,7 @@ def main():
 		line = line.strip("\r\n")
 		data.append(line.split(','))
 	data.remove([])
-	tree = {'color1R': {'3': 'NOT RIPE', '2': 'NOT RIPE', '5': {'color1B': {'5': {'color1G': {'3': {'color2R': {'3': 'RIPE', '4': {'color2B': {'3': 'OVERRIPE', '4': 'RIPE'}}}}, '2': {'color2R': {'5': 'RIPE', '4': {'color2B': {'3': 'RIPE', '4': 'NOT RIPE'}}}}}}, '4': 'RIPE'}}, '4': 'OVERRIPE'}}
+	tree = {'color1R': {'3': {'color1B': {'2': {'color1G': {'1': {'color2R': {'3': 'NOT RIPE', '5': 'RIPE'}}, '2': 'OVERRIPE'}}, '4': 'NOT RIPE'}}, '2': 'NOT RIPE', '5': {'color1B': {'3': 'RIPE', '5': {'color1G': {'3': {'color2R': {'3': 'RIPE', '4': {'color2B': {'3': {'color2G': {'1': 'RIPE', '2': 'OVERRIPE'}}, '4': 'RIPE'}}}}, '2': {'color2R': {'5': 'RIPE', '4': {'color2B': {'3': 'RIPE', '4': 'NOT RIPE'}}}}, '4': 'RIPE'}}, '4': {'color1G': {'1': 'RIPE', '3': {'color2R': {'5': 'RIPE', '4': 'OVERRIPE'}}, '2': {'color2R': {'3': 'RIPE', '5': 'RIPE', '4': {'color2B': {'2': 'RIPE', '4': 'NOT RIPE'}}}}}}}}, '4': {'color1B': {'3': 'OVERRIPE', '5': 'NOT RIPE', '4': {'color1G': {'2': 'NOT RIPE', '5': 'RIPE'}}}}}}
 	attributes = ['color1R', 'color1B', 'color1G', 'color2R', 'color2B', 'color2G', 'color3R', 'color3B', 'color3G', 'class']
 	count = 0
 	for entry in data:
@@ -28,7 +28,7 @@ def main():
 				globalfile.resultRIPE = tempDict[value]
 				tempDict = tempDict[value]
 			else:
-				print "can't process input %s" % count
+				print( "can't process input %s" % (count))
 				result = "?"
 				break
 		print ("entry%s = %s" % (count, globalfile.resultRIPE))
